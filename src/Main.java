1e56310b23fe7a4105c97f66f21a6d5c1703b727
vukien <vukien104@gmail.com>
2018-07-14 21:02:25 +0700

    create project

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        //<kieu du lieu> ten_bien = value;
+        // kieu so
+        // ky tu
+        // boolean
+
+        // mang
+
+        // Cac phep toan +, -, *, /, %, +=, -=, *=, /=, ++, --
+
+        // re nhanh
+        // if - else
+
+        // switch - case
+
+        // loop
+        // for
+        // while
+//        while(true) {
+//
+//        }
+        Scanner scanner = new Scanner(System.in);
+        scanner.next();
+
+        Random random = new Random();
+        random.nextInt(4);
+    }
+
+}
