36688e51b175d435fa546d71111c295a82f04bf5
vukien <vukien104@gmail.com>
2018-07-14 21:51:40 +0700

    Bai 1

--- src/Main.java
+++ src/Main.java
@@ -4,33 +4,52 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        String[][] map = {
+                {"*", "*", "*", "*"},
+                {"*", "*", "*", "*"},
+                {"*", "*", "P", "*"},
+                {"*", "*", "*", "*"}
+        };
 
-
-        //<kieu du lieu> ten_bien = value;
-        // kieu so
-        // ky tu
-        // boolean
-
-        // mang
-
-        // Cac phep toan +, -, *, /, %, +=, -=, *=, /=, ++, --
-
-        // re nhanh
-        // if - else
-
-        // switch - case
-
-        // loop
-        // for
-        // while
-//        while(true) {
-//
-//        }
         Scanner scanner = new Scanner(System.in);
-        scanner.next();
 
-        Random random = new Random();
-        random.nextInt(4);
+        int playerX = 2;
+        int playerY = 2;
+
+        while (true) {
+            for (int i = 0; i < map.length; i++) {
+                for (int j = 0; j < map[i].length; j++) {
+                    System.out.print(map[j][i] + " ");
+                }
+                System.out.println();
+            }
+
+            System.out.println("Nhap ky tu ban phim: ");
+            String key = scanner.next();
+
+            map[playerX][playerY] = "*";
+
+            switch (key) {
+                case "w":
+                    playerY -= 1;
+                    break;
+                case "s":
+                    playerY += 1;
+                    break;
+                case "a":
+                    playerX -= 1;
+                    break;
+                case "d":
+                    playerX += 1;
+                    break;
+            }
+            if (playerY == -1) playerY = map.length - 1;
+            if (playerY == map.length) playerY = 0;
+            if (playerX == -1) playerX = map.length - 1;
+            if (playerX == map.length)  playerX = 0;
+
+            map[playerX][playerY] = "P";
+        }
     }
 
 }
